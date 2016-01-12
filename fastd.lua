61f3a76f1b76981fcb02fd4c2600ca5a81ca9af3
Dominik Heidler <dominik@heidler.eu>
2016-01-12 14:47:39 +0100

    Parse Handshake Package

--- fastd.lua
+++ fastd.lua
@@ -5,19 +5,86 @@ local fastd_types = {
 	[2] = "Payload Packet",
 }
 
+local handshake_types = {
+	[1] = "request",
+	[2] = "reply",
+	[3] = "finish",
+}
+
+local reply_codes = {
+	[0] = "success",
+	[1] = "mandatory record missing",
+	[2] = "unacceptable value",
+}
+
+local modi = {
+	[0] = "TAP mode",
+	[1] = "TUN mode",
+}
+
 local f = fastd_proto.fields
 
 f.type = ProtoField.uint8("fastd.type", "Type", base.HEX, fastd_types)
 
+f.handshake_type = ProtoField.uint8("fastd.handshake_type", "Handshake type", base.HEX, handshake_types)
+f.reply_code = ProtoField.uint8("fastd.reply_code", "Reply Code", base.HEX, reply_codes)
+f.mode = ProtoField.uint8("fastd.mode", "Mode", base.HEX, modi)
+f.proto_name = ProtoField.string("fastd.proto_name", "Protocol name", FT_STRING)
+f.version_name = ProtoField.string("fastd.version_name", "Version name", FT_STRING)
+f.mtu = ProtoField.uint16("fastd.mtu", "MTU", base.DEC)
+f.method_name = ProtoField.string("fastd.method_name", "Method name", FT_STRING)
+f.method_list = ProtoField.string("fastd.method_list", "Method list", FT_STRING)
+f.sender_key = ProtoField.bytes("fastd.sender_key", "Sender key", base.HEX)
+f.recpt_key = ProtoField.bytes("fastd.recpt_key", "Recipient key", base.HEX)
+f.sender_hskey = ProtoField.bytes("fastd.sender_hskey", "Sender Handshake key", base.HEX)
+f.recpt_hskey = ProtoField.bytes("fastd.recpt_hskey", "Recipient Handshake key", base.HEX)
+
 function fastd_proto.dissector(buffer, pinfo, tree)
-   local subtree = tree:add(fastd_proto, buffer)
-   subtree:add(f.type, buffer(0,1))
+	local subtree = tree:add(fastd_proto, buffer)
+	subtree:add(f.type, buffer(0,1))
+	if buffer(0,1):uint() == 1 then
+		tlv_records_len = buffer(2,2):uint()
+		offset = 4
+		while (offset-4) < tlv_records_len do
+			record_type = buffer(offset,2):le_uint()
+			record_length = buffer(offset+2,2):le_uint()
+			offset = offset + 4
+			if record_type == 0x00 then
+				subtree:add(f.handshake_type, buffer(offset,record_length))
+			elseif record_type == 0x01 then
+				subtree:add(f.reply_code, buffer(offset,record_length))
+			elseif record_type == 0x04 then
+				subtree:add(f.mode, buffer(offset,record_length))
+			elseif record_type == 0x05 then
+				subtree:add(f.proto_name, buffer(offset,record_length))
+			elseif record_type == 0x06 then
+				subtree:add(f.sender_key, buffer(offset,record_length))
+			elseif record_type == 0x07 then
+				subtree:add(f.recpt_key, buffer(offset,record_length))
+			elseif record_type == 0x08 then
+				subtree:add(f.sender_hskey, buffer(offset,record_length))
+			elseif record_type == 0x09 then
+				subtree:add(f.recpt_hskey, buffer(offset,record_length))
+			elseif record_type == 0x0B then
+				subtree:add_le(f.mtu, buffer(offset,record_length))
+			elseif record_type == 0x0C then
+				subtree:add(f.method_name, buffer(offset,record_length))
+			elseif record_type == 0x0D then
+				subtree:add(f.version_name, buffer(offset,record_length))
+			elseif record_type == 0x0E then
+				subtree:add(f.method_list, buffer(offset,record_length))
+			else
+				offset = tlv_records_len + 4
+			end
+			offset = offset + record_length
+		end
+	elseif buffer(0,1):uint() == 2 then
+		ethernet_dissector = Dissector.get("eth")
 
-   ethernet_dissector = Dissector.get("eth")
-
-   -- skip over the header in front of the encapsulated ethernet frame
-   sub_buf = buffer(1, buffer:len() - 1):tvb()
-   ethernet_dissector:call(sub_buf, pinfo, tree)
+		-- skip over the header in front of the encapsulated ethernet frame
+		sub_buf = buffer(1, buffer:len() - 1):tvb()
+		ethernet_dissector:call(sub_buf, pinfo, tree)
+	end
 end
 
 -- load the udp port table
@@ -26,3 +93,4 @@ udp_table = DissectorTable.get("udp.port")
 -- register our protocol                                  
 udp_table:add(10004, fastd_proto)
 udp_table:add(10005, fastd_proto)
+udp_table:add(10014, fastd_proto)
